26b3cf1fd5f698d988fd9048e60755eef5574991
Hsieh <ss77995ss@gmail.com>
2018-11-08 16:11:27 +0800

    Add production server file to build a server without webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import app from './server/index';
+import app from './server/dev-server';
 
 const server = http.createServer(app);
 server.listen(process.env.PORT || 3000, (error) => {
